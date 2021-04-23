8bd295d2a8d8a2eeebded374cf8af954e50dcd00
Dri-28 <adrianacorreia1991@gmail.com>
2021-04-23 18:46:31 -0300

    projeto nlw

--- database/migrations/1619057426150-CreateSettings.ts
+++ database/migrations/1619057426150-CreateSettings.ts
@@ -22,7 +22,7 @@ export class CreateSettings1619057426150 implements MigrationInterface {
                         default:true
                     },
                     {
-                        name: "update_at",
+                        name: "updated_at",
                         type: "timestamp",
                         default:"now()"
                     },
